2b21084016b826c8ed4035565311916a4d6f079c
Gustavo Souza <78645956+gustavelson@users.noreply.github.com>
2021-11-30 11:04:54 -0300

    Add files via upload

---
+++ conversorfhtocelsius.cpp
@@ -0,0 +1,12 @@
+#include
+using namespace std;
+int main()
+{
+	float tempF, tempC;
+	count<<"Temperatura em Fahrenheit: "; 
+	cin>> tempF;
+	tempC=(tempF-32)/1.8;
+	count<<"Temperatura em graus Celsius: " << tempC;
+}
+
+
